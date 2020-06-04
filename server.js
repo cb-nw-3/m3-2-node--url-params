c46799a0e12168b455b28fd304905d4f87ce964c
Andres Rueda <garo99@gmail.com>
2020-06-04 13:19:37 -0400

    1.6 Solved

--- server.js
+++ server.js
@@ -1,35 +1,52 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
-const { top50 } = require("./data/top50");
-const { popularArtist } = require("./data/popularArtist");
+const { top50 } = require('./data/top50');
+const { popularArtist } = require('./data/popularArtist');
 
 const PORT = process.env.PORT || 8000;
-const home = (req, res) => res.render("pages/home");
-const top50page = (req, res) => res.render("pages/top50", { top50 });
+const home = (req, res) => res.render('pages/home');
+const top50page = (req, res) => res.render('pages/top50', { top50 });
+const songPage = (req, res) => {
+  const path = req.params[0].split('/');
+  // console.log(path[0]);
+  // console.log(path[1]);
+
+  if (path[0] === 'song' && path.length === 2) {
+    const song = top50.find((element) => element.rank === parseInt(path[1]));
+    res.render('pages/song', { song });
+  } else {
+    res.render('pages/fourOhFour', {
+      title: 'I got nothing',
+      path: req.originalUrl,
+    });
+  }
+};
+
 const songsOfMostPopular = (req, res) =>
-  res.render("pages/popular", { popularArtist });
+  res.render('pages/popular', { popularArtist });
 
 const app = express();
 
-app.use(morgan("dev"));
-app.use(express.static("public"));
+app.use(morgan('dev'));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // endpoints here
 
-app.get("/", home);
-app.get("/top50", top50page);
-app.get("/popular-artist", songsOfMostPopular);
+app.get('/', home);
+app.get('/top50', top50page);
+app.get('/top50/*', songPage);
+app.get('/top50/popular-artist', songsOfMostPopular);
 
 // handle 404s
-app.get("*", (req, res) => {
+app.get('*', (req, res) => {
   res.status(404);
-  res.render("pages/fourOhFour", {
-    title: "I got nothing",
+  res.render('pages/fourOhFour', {
+    title: 'I got nothing',
     path: req.originalUrl,
   });
 });
