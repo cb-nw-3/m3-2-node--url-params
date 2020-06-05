f00768d4157f2fbb3fc11c57b2ee24aefaac56bf
Mae <maeberris@outlook.com>
2020-06-05 16:24:53 -0400

    completed number 1

--- server.js
+++ server.js
@@ -16,11 +16,64 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
+
 app.get('/top50', function (req,res){
     res.render('pages/top50',{
         title: 'Top 50 Songs Streamed On Spotify',
         top50: top50,
     });
+})
+
+app.get('/top50/popular-artist', function(req,res){
+    let artistCount = {}
+
+    top50.forEach((song) =>{
+        if(artistCount[song.artist] === undefined){
+            artistCount[song.artist] = 1;
+        }else{
+            artistCount[song.artist] += 1;
+        }
+    })
+    let artistArray = Object.keys(artistCount)
+
+    let sortedArtistArray = artistArray.sort((artistA, artistB)=>{
+        let countA = artistCount[artistA]
+        let countB = artistCount[artistB]
+        if(countA < countB){
+            return 1
+        }else{
+            return -1
+        }
+    })
+    console.log(sortedArtistArray)
+
+    let mostPopularArtist = sortedArtistArray[0]
+
+    let onlyPopularArtist = top50.filter((item) =>{
+        if(item.artist === mostPopularArtist){
+            return item
+        }
+    })
+
+    res.render('pages/top50', {
+        title:'Most Popular Artist',
+        top50: onlyPopularArtist,
+    })
+})
+
+app.get('/top50/song/:num', (req,res) =>{
+    let songRank = req.params.num - 1
+    if(songRank >= 0 && songRank <= 49){
+        res.render('pages/song-page', {
+            selectedSong: top50[songRank],
+            title: 'Song # '+ (songRank + 1)
+        })
+    }else{
+        res.render('pages/fourOhFour', {
+            title: 'I got nothing',
+            path: req.originalUrl
+        });
+    }
 })
 
 // endpoints here
