9646daaffad0a3deaec316d023f0f5951906f6a9
donnyesq <adonis@darmas.ca>
2020-06-04 20:39:51 -0400

    excercise 2.1, 2nd requirement complete

--- server.js
+++ server.js
@@ -44,6 +44,17 @@ const findSong = (arr, rank) => {
   return currentSong;
 };
 
+const findBook = (arr, id) => {
+  let currentBook = [];
+
+  arr.forEach((book) => {
+    if (book.id === parseInt(id)) {
+      currentBook.push(book);
+    }
+  });
+  return currentBook;
+};
+
 const express = require("express");
 
 const morgan = require("morgan");
@@ -66,12 +77,17 @@ const songTitle = `Song #`;
 
 const bookListTitle = "Books";
 
+const bookTitle = "Books #";
+
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
 // endpoints here
+
+// SONGS endpoints
+
 app.get("/top50", (req, res) => {
   res.render("pages/top50.ejs", { title: title, top50: top50 });
 });
@@ -98,8 +114,25 @@ app.get("/top50/song/:rank", (req, res) => {
   }
 });
 
+// BOOKS endpoints
+
 app.get("/books", (req, res) => {
   res.render("pages/bookList.ejs", { title: bookListTitle, books: books });
+});
+
+app.get("/books/:id", (req, res) => {
+  if (req.params.id > 100 && req.params.id < 126) {
+    res.render("pages/bookPage.ejs", {
+      title: bookTitle + req.params.id,
+      books: findBook(books, req.params.id),
+    });
+  } else {
+    res.status(404);
+    res.render("pages/fourOhFour", {
+      title: "I got nothing",
+      path: req.originalUrl,
+    });
+  }
 });
 
 // handle 404s
