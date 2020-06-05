f205f903598495ca54453021a7db5368e83e496c
Aquarius-beans <rare.beans1959@gmail.com>
2020-06-05 18:47:06 -0400

    commit

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 'use strict';
-
+const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
 const { top50 } = require('./data/top50');
-
 const PORT = process.env.PORT || 8000;
-
 const app = express();
-
 app.use(morgan('dev'));
 app.use(express.static('public'));
+app.use(bodyParser.json());
 app.use(express.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 
-// endpoints here
+app.get('/top50', (req, res) => {
+    res.render('pages/top50', {
+        title: 'Top 50 Songs Streamed on Spotify',
+        top50:top50,
+    });
+});
 
 // handle 404s
-app.git('*', (req, res) => {
+app.get('*', (req, res) => {
     res.status(404);
     res.render('pages/fourOhFour', {
         title: 'I got nothing',
@@ -24,4 +27,4 @@ app.git('*', (req, res) => {
     });
 });
 
-get.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
