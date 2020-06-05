d8c6fd6ffb3e9fc9692cbe33c332adc0cce5ab15
jenny ma <jenny_lau_8@hotmail.com>
2020-06-04 23:06:52 -0400

    until 1.4

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 
 const morgan = require('morgan');
 
+//##1.2 requiring the 'top50' file
 const { top50 } = require('./data/top50');
+
+
 
 const PORT = process.env.PORT || 8000;
 
@@ -15,6 +18,13 @@ app.use(express.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 
 // endpoints here
+app.get('/top50', (req, res) => {
+    res.render('pages/top50', {
+        title: 'Top 50 Songs Streamed on Spotify',
+        top50: top50
+    });
+})
+
 
 // handle 404s
 app.get('*', (req, res) => {
