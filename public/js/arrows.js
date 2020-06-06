4dd1f855f5c97c4959a2173b7ccf82464529583a
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-06-05 22:48:57 -0400

    2.2 Solved by their type

--- public/js/arrows.js
+++ public/js/arrows.js
@@ -3,12 +3,6 @@ const arrowRight = document.body.querySelector("#arrow-right-song");
 arrowLeft.addEventListener("click", left);
 arrowRight.addEventListener("click", right);
 
-const arrowLeftBook = document.body.querySelector("#arrow-left-book");
-const arrowRightBook = document.body.querySelector("#arrow-right-book");
-
-arrowLeftBook.addEventListener("click", leftB);
-arrowRightBook.addEventListener("click", rightB);
-
 function left(e) {
   const path = e.srcElement.baseURI.split("/");
   let newPage = parseInt(path[path.length - 1]);
@@ -22,30 +16,6 @@ function left(e) {
 }
 
 function right(e) {
-  const path = e.srcElement.baseURI.split("/");
-  let newPage = parseInt(path[path.length - 1]);
-  newPage++;
-  if (newPage < 101 || newPage > 125) {
-    newPage = parseInt(path[path.length - 1]);
-  }
-  const newRoute = path.slice(0, path.length - 1);
-  newRoute.push(newPage);
-  location.replace(newRoute.join("/"));
-}
-
-function leftB(e) {
-  const path = e.srcElement.baseURI.split("/");
-  let newPage = parseInt(path[path.length - 1]);
-  newPage--;
-  if (newPage < 101 || newPage > 125) {
-    newPage = parseInt(path[path.length - 1]);
-  }
-  const newRoute = path.slice(0, path.length - 1);
-  newRoute.push(newPage);
-  location.replace(newRoute.join("/"));
-}
-
-function rightB(e) {
   const path = e.srcElement.baseURI.split("/");
   let newPage = parseInt(path[path.length - 1]);
   newPage++;
--- server.js
+++ server.js
@@ -1,51 +1,56 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const { top50 } = require('./data/top50');
-const { books } = require('./data/books');
-const { popularArtist } = require('./data/popularArtist');
+const { top50 } = require("./data/top50");
+const { books } = require("./data/books");
+const { popularArtist } = require("./data/popularArtist");
+let bookTypes = {};
+books.forEach((element) =>
+  element.type in bookTypes
+    ? bookTypes[`${element.type}`]++
+    : (bookTypes[`${element.type}`] = 1)
+);
 
 const PORT = process.env.PORT || 8000;
-const home = (req, res) => res.render('pages/home');
-const top50page = (req, res) => res.render('pages/top50', { top50 });
+const home = (req, res) => res.render("pages/home");
+const top50page = (req, res) => res.render("pages/top50", { top50 });
 const songPage = (req, res) => {
-  const path = req.params[0].split('/');
-
+  const path = req.params[0];
   if (
-    path[0] === 'song' &&
-    path.length === 2 &&
-    parseInt(path[1]) >= 1 &&
-    parseInt(path[1]) <= 50 &&
-    path[1].length <= 2
+    parseInt(path[0]) >= 1 &&
+    parseInt(path[0]) <= 50 &&
+    path[0].length <= 2
   ) {
-    const song = top50.find((element) => element.rank === parseInt(path[1]));
-    res.render('pages/song', { song });
+    const song = top50.find((element) => element.rank === parseInt(path[0]));
+    res.render("pages/song", { song });
   } else if (path.length <= 3) {
-    res.render('pages/fourOhFour', {
-      title: 'I got nothing',
+    res.render("pages/fourOhFour", {
+      title: "I got nothing",
       path: req.originalUrl,
     });
   }
 };
 
 const songsOfMostPopular = (req, res) =>
-  res.render('pages/popular', { popularArtist });
+  res.render("pages/popular", { popularArtist });
 
-const bookList = (req, res) => res.render('pages/bookList', { books });
+const bookList = (req, res) => res.render("pages/bookList", { books });
+const bookType = (req, res) =>
+  res.render("pages/bookType", { books, bookTypes });
 const bookPage = (req, res) => {
-  const path = req.params[0].split('/');
+  const path = req.params[0].split("/");
   if (
     parseInt(path[0]) >= 100 &&
     parseInt(path[0]) <= 125 &&
     path[0].length === 3
   ) {
     const book = books.find((element) => element.id === parseInt(path[0]));
-    res.render('pages/book', { book });
+    res.render("pages/book", { book });
   } else {
-    res.render('pages/fourOhFour', {
-      title: 'I got nothing',
+    res.render("pages/fourOhFour", {
+      title: "I got nothing",
       path: req.originalUrl,
     });
   }
@@ -53,26 +58,27 @@ const bookPage = (req, res) => {
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.static('public'));
+app.use(morgan("dev"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // endpoints here
 
-app.get('/', home);
-app.get('/top50', top50page);
-app.get('/top50/*', songPage);
-app.get('/top50/popular-artist', songsOfMostPopular);
+app.get("/", home);
+app.get("/top50", top50page);
+app.get("/top50/song/*", songPage);
+app.get("/top50/popular-artist", songsOfMostPopular);
 
-app.get('/books', bookList);
-app.get('/books/*', bookPage);
+app.get("/books", bookList);
+app.get("/books/bytype", bookType);
+app.get("/book/*", bookPage);
 
 // handle 404s
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
   res.status(404);
-  res.render('pages/fourOhFour', {
-    title: 'I got nothing',
+  res.render("pages/fourOhFour", {
+    title: "I got nothing",
     path: req.originalUrl,
   });
 });
