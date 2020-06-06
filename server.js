854f06d6079ed7fad09ce32d1d681d60e1c0a200
jenny ma <jenny_lau_8@hotmail.com>
2020-06-06 11:22:41 -0400

    E1.6

--- server.js
+++ server.js
@@ -53,6 +53,22 @@ app.get("/top50/popular-artist", (req, res) => {
     });
   });
 
+//#1.6
+app.get('/top50/song/:rank', (req, res) => {
+    const rank = req.params.rank - 1;
+    if (top50[rank]) {    
+        res.render('pages/songPage', {
+            title: `Song #${top50[rank].rank}`,
+            song: top50[rank]
+        });
+    } else {
+        res.status(404);
+        res.render('pages/fourOhFour', {
+            title: 'I got nothing',
+            path: req.originalUrl
+        });
+    }
+});
 
 
 // handle 404s
