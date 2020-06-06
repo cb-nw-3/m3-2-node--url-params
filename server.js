aeb704a0124fb17dd07f1a944ba86e33e876e626
Mae <maeberris@outlook.com>
2020-06-05 21:59:48 -0400

    completed exercise 2

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const nodemon = require('nodemon')
 
 const { top50 }  = require('./data/top50');
 
+const { books } = require('./data/books')
+
 const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -21,6 +23,7 @@ app.get('/top50', function (req,res){
     res.render('pages/top50',{
         title: 'Top 50 Songs Streamed On Spotify',
         top50: top50,
+        style: 'styles'
     });
 })
 
@@ -58,6 +61,7 @@ app.get('/top50/popular-artist', function(req,res){
     res.render('pages/top50', {
         title:'Most Popular Artist',
         top50: onlyPopularArtist,
+        style: 'styles'
     })
 })
 
@@ -66,15 +70,64 @@ app.get('/top50/song/:num', (req,res) =>{
     if(songRank >= 0 && songRank <= 49){
         res.render('pages/song-page', {
             selectedSong: top50[songRank],
-            title: 'Song # '+ (songRank + 1)
+            title: 'Song # '+ (songRank + 1),
+            style: 'styles'
         })
     }else{
         res.render('pages/fourOhFour', {
             title: 'I got nothing',
-            path: req.originalUrl
+            path: req.originalUrl,
+            style: 'styles'
         });
     }
 })
+
+app.get('/books', (res,req) =>{
+    req.render('pages/allBooks', {
+        books: books,
+        style: 'books',
+        title:'Top Books to Read'
+    })
+})
+
+app.get('/books/:id', (req,res) =>{
+    let bookId = req.params.id
+    let bookPlacement = bookId -101
+    
+    // console.log(bookId)
+    // let findBookMatch = books.filter((book) => {
+    //     if(book.id === bookId){
+    //         return book
+    //     }
+    // })
+    // console.log(findBookMatch)
+
+    // let book = findBookMatch[0]
+
+    res.render('pages/singleBook', {
+        book: books[bookPlacement],
+        style: 'single-book',
+        title: books[bookPlacement].title
+    })
+})
+
+app.get('/books/type/:type', (req, res) =>{
+    let type = req.params.type
+
+    let filteredArray = books.filter((book) =>{
+        if(book.type === type){
+            return book
+        }
+    })
+    res.render('pages/bookTypes', {
+        books: filteredArray,
+        style: 'booktype',
+        title: type + 'books',
+        type: type
+    })
+})
+
+
 
 // endpoints here
 
@@ -83,7 +136,8 @@ app.get('*', (req, res) => {
     res.status(404);
     res.render('pages/fourOhFour', {
         title: 'I got nothing',
-        path: req.originalUrl
+        path: req.originalUrl,
+        style: 'styles'
     });
 });
 
