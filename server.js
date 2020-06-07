0f241c1f8b5222b0055e183da7994f5d428e8397
Przemek Piwek <przemekpiwek@gmail.com>
2020-06-07 14:43:04 -0600

    Finished Exercise

--- server.js
+++ server.js
@@ -28,6 +28,29 @@ app.get("/popular-artist", (req, res) => {
     top50: top50,
   });
 });
+app.get("/top50/song/:id", (req, res) => {
+  let songNumber = req.params.id;
+
+  if (top50[songNumber]) {
+    let title = top50[songNumber - 1].title;
+    let artist = top50[songNumber - 1].artist;
+    let streams = top50[songNumber - 1].streams;
+
+    res.render("pages/song", {
+      songNumber: songNumber,
+      top50: top50,
+      title: title,
+      artist: artist,
+      streams: streams,
+    });
+  } else {
+    res.status(404);
+    res.render("pages/fourOhFour", {
+      title: "I got nothing",
+      path: req.originalUrl,
+    });
+  }
+});
 
 // handle 404s
 app.get("*", (req, res) => {
