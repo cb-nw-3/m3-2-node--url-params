c23b60e7b50d13d73349e87994540779e4388b13
Philppe <psalvail@assetscience.com>
2020-06-07 19:35:31 -0400

    M3-2 Node completed with exercise 2

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
 const express = require("express");
 const morgan = require("morgan");
 const { top50 } = require("./data/top50");
+const { books } = require("./data/books");
 const PORT = process.env.PORT || 8000;
 
 const q1 = (req, res) => {
   const title = "Top 50 Songs Streamed on Spotify";
   res.render("./pages/top50", { title, top50 });
 };
-
 const q2 = (req, res) => {
   const title = "Top 50 Songs Streamed on Spotify";
   let artistMentioned = [];
@@ -40,7 +40,8 @@ const q3 = (req, res) => {
   const songID = req.params.songID;
   if (songID < 1 || songID > top50.length) {
     let title = "I got Nothing";
-    res.render("./pages/fourOhFour", { title, songID });
+    let id = songID;
+    res.render("./pages/fourOhFour", { title, id });
   } else {
     let songSelected = {};
     top50.forEach(function (song) {
@@ -49,20 +50,56 @@ const q3 = (req, res) => {
       }
     });
     let title = "Song #" + songID;
-    console.log("songSelected: ", songSelected);
+
     res.render("./pages/songPage", { title, songSelected });
   }
 };
 
-const app = express();
+const q4 = (req, res) => {
+  const title = "Here are your Books";
+  res.render("./pages/allBooks", { title, books });
+};
 
-// app.get("/", function (req, res) {
-//   res.send("You are at the Home Page!!!");
-// });
+//
+const q5 = (req, res) => {
+  const bookID = req.params.bookID;
+  if (bookID < 101 || bookID > 125) {
+    let title = "I got Nothing";
+    let id = bookID;
+    res.render("./pages/fourOhFour", { title, id });
+  } else {
+    let bookSelected = {};
+    books.forEach(function (book) {
+      if (book.id == bookID) {
+        bookSelected = book;
+      }
+    });
+    const title = "Book info for ID: " + bookID;
+    res.render("./pages/bookPage", { title, bookSelected });
+  }
+};
+const q6 = (req, res) => {
+  const bookType = req.params.type;
+  console.log("bookType", bookType);
+  let bookTypes = [];
+  bookTypes = books.filter(function (book) {
+    return book.type == bookType;
+  });
+  const title = "Books of Type: " + bookType;
+  res.render("./pages/bookType", { title, bookTypes });
+};
+
+const app = express();
 
 app.get("/top50", q1);
 app.get("/top50/popular-artist", q2);
 app.get("/top50/:songID", q3);
+app.get("/books", q4);
+app.get("/books/:bookID", q5);
+app.get("/books/bookTypes/:type", q6);
+
+// app.get("/books/:songID", q5);
+
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
