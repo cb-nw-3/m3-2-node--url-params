7575a8c8ae457c0308c39663756620d4d1477fef
Nathalie Benarroch <nathaliebenarroch@Nathalies-MacBook-Pro.local>
2020-06-07 11:33:05 -0400

    finished 1

--- server.js
+++ server.js
@@ -16,12 +16,39 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
-// endpoints
+// endpoints here
 app.get("/top50", (req, res) => {
   res.render("pages/top50", {
     title: "Top 50 Songs Streamed on Spotify",
     top50: top50,
   });
+});
+
+// revoir avec guillaume
+app.get("/top50/popular-artists", (req, res) => {
+  const isThisAJustinBieberSong = (song) => song.artist === "Justin Bieber";
+  const songs = top50.filter(isThisAJustinBieberSong);
+  res.render("pages/popularArtists", {
+    title: "Popular Artists",
+    songs: songs,
+  });
+});
+
+app.get("/song/:song", (req, res) => {
+  const isTheSongFromParams = (song) => song.rank === Number(req.params.song);
+  const song = top50.find(isTheSongFromParams);
+  if (song) {
+    res.render("pages/songPage", {
+      title: "Song details",
+      song: song,
+    });
+  } else {
+    res.status(404);
+    res.render("pages/fourOhFour", {
+      title: "I got nothing",
+      path: req.originalUrl,
+    });
+  }
 });
 
 // handle 404s
