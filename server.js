6de10db9e1cd97a8f3662993cc478490968efd47
Philppe <psalvail@assetscience.com>
2020-06-07 14:46:14 -0400

    Completed 1.5 and 1.6

--- server.js
+++ server.js
@@ -10,12 +10,54 @@ const q1 = (req, res) => {
   res.render("./pages/top50", { title, top50 });
 };
 
+const q2 = (req, res) => {
+  const title = "Top 50 Songs Streamed on Spotify";
+  let artistMentioned = [];
+  let artistPopularity = [];
+  top50.forEach((musician) => {
+    if (!artistMentioned.includes(musician.artist)) {
+      artistMentioned.push(musician.artist);
+      musician.points = 0;
+      artistPopularity.push(musician);
+    } else {
+      artistPopularity.forEach((popular) => {
+        if (popular.artist === musician.artist) {
+          popular.points += musician.streams;
+        }
+      });
+    }
+  });
+  artistPopularity.sort(function (a, b) {
+    return b.points - a.points;
+  });
+  let finalPopList = top50.filter(function (item) {
+    return item.artist === artistPopularity[0].artist;
+  });
+  res.render("./pages/popular-artist", { title, finalPopList });
+};
+
+const q3 = (req, res) => {
+  const songID = req.params.songID;
+  let songSelected = {};
+  top50.forEach(function (song) {
+    if (song.rank == songID) {
+      songSelected = song;
+    }
+  });
+  let title = "Song #" + songID;
+  console.log("songSelected: ", songSelected);
+  res.render("./pages/songPage", { title, songSelected });
+};
+
 const app = express();
 
 // app.get("/", function (req, res) {
 //   res.send("You are at the Home Page!!!");
 // });
-app.get("/", q1);
+
+app.get("/top50", q1);
+app.get("/top50/popular-artist", q2);
+app.get("/top50/:songID", q3);
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
