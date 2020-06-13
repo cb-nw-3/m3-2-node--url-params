9a949aa23d9e6745b4094d76059a7b45ea7f3edb
anwesh-sarkar <anwesh.sarkar@gmail.com>
2020-06-13 11:51:08 -0400

    Finished 1.5

--- server.js
+++ server.js
@@ -22,6 +22,48 @@ app.get("/top50", (req, res) => {
   });
 });
 
+app.get("/top50/popular-artist", (req, res) => {
+  let allArtistsObj = {};
+
+  // Create an object which contains each artist and count the number of times they appear
+  top50.forEach((song) => {
+    if (allArtistsObj[song.artist] === undefined) {
+      allArtistsObj[song.artist] = 1;
+    } else {
+      allArtistsObj[song.artist] += 1;
+    }
+  });
+
+  // Create an array containing the names of the array
+  let keysArray = Object.keys(allArtistsObj);
+
+  // Create an array that is sorted based by the artist with the highest count
+  let sortedArray = keysArray.sort((artistA, artistB) => {
+    let numOfA = allArtistsObj[artistA];
+    let numOfB = allArtistsObj[artistB];
+
+    if (numOfA > numOfB) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  // Find the artist with the highest count
+  let mostPopularArtist = sortedArray[0];
+
+  res.render("pages/popular-artist", {
+    title: "Most Popular Artist",
+
+    // Display page with filtering the songs of the artist with the highest count
+    top50: top50.filter((song) => {
+      if (song.artist === mostPopularArtist) {
+        return song;
+      }
+    }),
+  });
+});
+
 // handle 404s
 app.get("*", (req, res) => {
   res.status(404);
